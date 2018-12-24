3ba871b4be1841eae3260a494f3f352279cd3eda
Jon Roobottom <roo@roodesign.co.uk>
2018-12-24 14:58:40 +0000

    updates

---
+++ site/addons/NoParagraphFigure/NoParagraphFigureModifier.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Statamic\Addons\NoParagraphFigure;
+
+use Statamic\Extend\Modifier;
+
+class NoParagraphFigureModifier extends Modifier
+{
+    /**
+     * Modify a value
+     *
+     * @param mixed  $value    The value to be modified
+     * @param array  $params   Any parameters used in the modifier
+     * @param array  $context  Contextual values
+     * @return mixed
+     */
+    public function index($value, $params, $context)
+    {
+        $pattern = '/(<p><figure)(.+?)(<\/figure>\s*<\/p>)/i';
+        $replacement = '<figure$2</figure>';
+        return preg_replace($pattern, $replacement, $value);
+    }
+}
